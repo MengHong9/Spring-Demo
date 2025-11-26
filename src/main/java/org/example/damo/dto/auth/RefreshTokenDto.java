d63aae5f4450e8688b53c6f48d8dd3691980ff1a
Mr.Frog <Bunmenghong7@gmail.com>
2025-11-26 09:36:06 +0700

    refactor: add validation for request token

--- src/main/java/org/example/damo/dto/auth/RefreshTokenDto.java
+++ src/main/java/org/example/damo/dto/auth/RefreshTokenDto.java
@@ -1,10 +1,15 @@
 package org.example.damo.dto.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public class RefreshTokenDto {
 
     @JsonProperty("refresh_token")
+    @NotNull(message = "refresh token is required")
+    @NotEmpty(message = "refresh token should not be empty")
     String refreshToken;
 
     public String getRefreshToken() {
