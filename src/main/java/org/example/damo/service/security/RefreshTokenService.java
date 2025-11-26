4a9f518cca283d2ac3aca02567209c396164c63a
Mr.Frog <Bunmenghong7@gmail.com>
2025-11-26 08:30:51 +0700

    refactor(refresh-token): generate with secure random

--- src/main/java/org/example/damo/service/security/RefreshTokenService.java
+++ src/main/java/org/example/damo/service/security/RefreshTokenService.java
@@ -5,10 +5,13 @@ import org.example.damo.entity.User;
 import org.example.damo.exception.model.ResourceNotFoundException;
 import org.example.damo.repository.RefreshTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.naming.AuthenticationException;
+import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.UUID;
 
 @Service
@@ -16,14 +19,17 @@ public class RefreshTokenService {
     @Autowired
     private RefreshTokenRepository refreshTokenRepository;
 
+    @Value("${config.security.refresh-token-expiration}")
+    private Long expiration;
+
     public RefreshToken createRefreshToken(User user) {
 
-        // TODO : generate as Base64 URL Encoder
-        String refreshToken = UUID.randomUUID().toString();
+
+        String refreshToken = this.generateSecureRefreshToken();
 
         RefreshToken refreshTokenEntity = new RefreshToken();
         refreshTokenEntity.setToken(refreshToken);
-        refreshTokenEntity.setExpiresAt(LocalDateTime.now().plusHours(3));
+        refreshTokenEntity.setExpiresAt(LocalDateTime.now().plusHours(this.expiration));
         refreshTokenEntity.setUser(user);
 
         return refreshTokenRepository.save(refreshTokenEntity);
@@ -48,5 +54,17 @@ public class RefreshTokenService {
         refreshTokenRepository.save(oldToken);
 
         return this.createRefreshToken(oldToken.getUser());
+    }
+
+    private String generateSecureRefreshToken() {
+        SecureRandom random = new SecureRandom();
+
+        //array bytes of 64 length
+        byte[] tokenBytes = new byte[64];
+
+        //make each byte has its own secure value
+        random.nextBytes(tokenBytes);
+
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(tokenBytes);
     }
 }
